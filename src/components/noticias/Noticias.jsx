57bf73ac9e896084a2e99bf016815494f28a7b54
danielay <danielitayero@gmail.com>
2023-04-16 19:40:08 +0200

    add: noticias

---
+++ src/components/noticias/Noticias.jsx
@@ -0,0 +1,51 @@
+import "./Noticias.css"
+
+function Noticias() {
+    return <>
+        <div className = "NewsBx">
+            <div className = "New">
+                <div className = "titular"> 
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+                <div className= "date"> <h4>Fecha</h4> </div>
+                <div className = "img2">
+                    <img src="" alt="" />
+                </div>
+            </div>
+
+            <div className = "New">
+                <div className = "titular"> 
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+                <div className= "date"> <h4>Fecha</h4> </div>
+                <div className = "img2">
+                    <img src="" alt="" />
+                </div>
+            </div>
+
+            <div className = "New">
+                <div className = "titular"> 
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+                <div className= "date"> <h4>Fecha</h4>  </div>
+                <div className = "img2">
+                    <img src="" alt="" />
+                </div>
+            </div>
+
+            <div className = "New">
+                <div className = "titular"> 
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+                <div className= "date"> <h4>Fecha</h4> </div>
+                <div className = "img2">
+                    <img src="" alt="" />
+                </div>
+            </div>
+            
+
+        </div>
+    </>
+}
+
+export default Noticias ;
