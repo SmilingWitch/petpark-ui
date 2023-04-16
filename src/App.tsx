983d365fb0167de1a6f539f3a642d3e538ee15a8
danielay <danielitayero@gmail.com>
2023-04-16 19:23:14 +0200

    add: App file

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import PrincipalPage from './components/principalPage/PrincipalPage'
+import Products from './components/products/Products'
+import Slide from './components/slide/Slide'
+
+
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <PrincipalPage/>
+    </div>
+    
+    
+      
+  )
+}
+
+export default App
