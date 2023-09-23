99c2ab60f662a0cacbd6a8058da1ecf40d100713
danielay <danielitayero@gmail.com>
2023-09-23 12:50:30 -0400

    feat: make noticias  responsive

--- src/components/noticias/Noticias.jsx
+++ src/components/noticias/Noticias.jsx
@@ -1,49 +1,35 @@
 import "./Noticias.css"
+import NoticeCard from "../noticeCard/NoticeCard"
+import {FiArrowUpRight} from "react-icons/fi"
+import RedButton from "../redButton/RedButton"
 
 function Noticias() {
     return <>
         <div className = "NewsBx">
-            <div className = "New">
-                <div className = "titular"> 
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <div className= "date"> <h4>Fecha</h4> </div>
-                <div className = "img2">
-                    <img src="" alt="" />
-                </div>
-            </div>
-
-            <div className = "New">
-                <div className = "titular"> 
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <div className= "date"> <h4>Fecha</h4> </div>
-                <div className = "img2">
-                    <img src="" alt="" />
-                </div>
-            </div>
-
-            <div className = "New">
-                <div className = "titular"> 
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <div className= "date"> <h4>Fecha</h4>  </div>
-                <div className = "img2">
-                    <img src="" alt="" />
-                </div>
-            </div>
-
-            <div className = "New">
-                <div className = "titular"> 
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <div className= "date"> <h4>Fecha</h4> </div>
-                <div className = "img2">
-                    <img src="" alt="" />
-                </div>
+            <div className="Bx1">
+                <div className="headerBx">
+                    <div className="header2">
+                        <h2>Noticias!!!</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque porro, nobis debitis nesciunt totam quia amet explicabo. Incidunt, consequuntur? Iure, voluptatem! Placeat quae sapiente quas adipisci, animi minus culpa modi!</p>
+                    </div>
+                    <div className="buttonNotice">
+                        <div className="allNotices"> <RedButton textButton="Todas las noticias"/></div>
+                        <div className="addNotices"> 
+                            <a href="#" >
+                                Agregar una noticia 
+                            </a> 
+                            <a href="#"><FiArrowUpRight/></a>
+                        </div>
+                    </div> 
+                </div>                     
             </div>
             
-
+            <div className="NoticeCardConeiner">
+            <NoticeCard/>
+            <NoticeCard/>
+            </div>
+            
+            
         </div>
     </>
 }
